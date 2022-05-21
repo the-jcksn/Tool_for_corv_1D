a1c88dfb38594fd88426c1290cd648e3b5371fce
the-jcksn <101604447+the-jcksn@users.noreply.github.com>
2022-05-21 10:04:47 +0100

    Add files via upload

---
+++ reddit.py
@@ -0,0 +1,22 @@
+#tool for random redditor
+
+user_input = input('Please enter the name of the input phrases to modify: ')
+all_output_phrases = []
+with open(user_input,'r') as phrases_file:
+    for phrase in phrases_file:
+        capitalised_list = []
+        new_phrase = phrase.replace('b','bb')
+        new_phrase = new_phrase.replace('e','3')
+        phrase_list = new_phrase.split(' ')
+        for word in phrase_list:
+            capitalised_last = word[-1:]
+            capitalised_last = capitalised_last.upper()
+            word = word[:-1] + capitalised_last
+            capitalised_list.append(word)
+        all_rules_applied = ' '.join(capitalised_list)
+        all_output_phrases.append(all_rules_applied)
+with open('output.txt', 'w') as output:
+    for line in all_output_phrases:
+        output.write(line)
+
+print('\nAll done, converted phrases written to output.txt')
